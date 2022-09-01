9986f8ecf276e833fa5696ac9af611f26c317259
pedrs0m <105890849+pedrs0m@users.noreply.github.com>
2022-09-01 19:42:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL("AniversarioImagem.png", function(img) {
+
+    blockImageObject = img;
+    blockImageObject.scaleToWidth(700);
+    blockImageObject.scaleToHeight(510);
+    blockImageObject.set({
+
+        top:0,
+        left:0
+    });
+canvas.add(blockImageObject);
+});
+
+
+
+
+	
+
+
+}
+
+function playSound(){
+x.play();
+}
